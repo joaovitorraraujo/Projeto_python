30120b7292164588354dddbd772c48fd6ba929e1
Joao Vitor Correia <joaovitor19.03.2004@gmail.com>
2023-10-16 23:38:00 -0300

    add_menu

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ from tkinter import PhotoImage
 from tkinter import RIGHT
 import function
 import re
+from PIL import Image
 
 lista_registers = []
 
@@ -109,26 +110,6 @@ class TelaLogin:
         window.iconbitmap("imagens/icon.ico")
 
     def screen_login(self):
-        def create_menu():
-            screen_menu = ctk.CTkToplevel(master=window)
-            screen_menu.title("Sistema de login")
-            screen_menu.geometry("1000x700")
-            screen_menu.resizable(True, True)
-            screen_menu.wm_iconbitmap("imagens/icon.ico")
-
-            def back_login():
-                # serve para mostrar a tela que tinha sido escondida
-                window.deiconify()
-                screen_menu.destroy()
-
-            button_sair = ctk.CTkButton(
-                master=screen_menu,
-                text=("Sair"),
-                text_color=("Black"),
-                fg_color=("Red"),
-                command=back_login,
-            )
-            button_sair.place(x=500, y=350)
 
         # imagem da janela
         img_log = PhotoImage(file="imagens/log.png")
@@ -187,7 +168,7 @@ class TelaLogin:
 
         def on_enter_pressed(event):
             event.widget.tk_focusNext().focus()
-
+    
         entry_email = ctk.CTkEntry(
             master=frame_login,
             placeholder_text="Digite seu E-mail ou usuário",
@@ -295,6 +276,160 @@ class TelaLogin:
             command=show_items,
         )
         button_forgot_password.place(x=225, y=250)
+        
+        def create_menu():
+            screen_menu = ctk.CTkToplevel(master=window)
+            screen_menu.title("Sistema de login")
+            screen_menu.geometry("1366x768")
+            screen_menu.resizable(True, True)
+            screen_menu.wm_iconbitmap("imagens/icon.ico")
+
+            home_image = ctk.CTkImage(
+                dark_image=Image.open("imagens/home_light.png"), size=(40, 40)
+            )
+            chat_image = ctk.CTkImage(
+                dark_image=Image.open("imagens/chat_light.png"), size=(40,40)
+            )
+            
+            img_options_menu = ctk.CTkImage(
+                dark_image=Image.open("imagens/user.png"), size=(40, 40)
+            )
+            img_add_user = ctk.CTkImage(
+                dark_image=Image.open("imagens/add_user_light.png"), size=(40, 40)
+            )
+
+            navigation_frame = ctk.CTkFrame(master=screen_menu, corner_radius=0,width=300 ,height=768)
+            navigation_frame.grid(row=0, column=0, sticky="nsew")
+            navigation_frame.grid_rowconfigure(4, weight=1)
+            
+            label_logo1 = ctk.CTkLabel(
+                master=navigation_frame,
+                text="Sistema ",
+                font=(
+                    "Impact",
+                    30,
+                ),
+                text_color="white",
+                bg_color="transparent",
+            )
+            label_logo1.place(x=20, y=20)
+
+            label_logo2 = ctk.CTkLabel(
+                master=navigation_frame,
+                text="De",
+                font=(
+                    "Impact",
+                    16,
+                ),
+                text_color="white",
+                bg_color="transparent",
+            )
+            label_logo2.place(x=129, y=29)
+
+            label_logo3 = ctk.CTkLabel(
+                master=navigation_frame,
+                text="Login",
+                font=("Impact", 59),
+                text_color="#0080e4",
+                bg_color="transparent",
+            )
+            label_logo3.place(x=20, y=45)
+
+            label_logo3.lift()
+            label_logo2.lift()
+            label_logo1.lift()
+            
+
+            home_button = ctk.CTkButton(
+                master=navigation_frame,
+                corner_radius=0,
+                height=40,
+                border_spacing=10,
+                text="Menu Principal",
+                fg_color="transparent",
+                text_color=("gray10", "gray90"),
+                hover_color=("gray70", "gray30"),
+                image=home_image,
+                anchor="w",
+            )
+            home_button.grid(row=1, column=0,pady=(120,0) ,sticky="ew")
+
+            chat_button = ctk.CTkButton(
+                navigation_frame,
+                corner_radius=0,
+                height=40,
+                border_spacing=10,
+                text="Chat",
+                fg_color="transparent",
+                text_color=("gray10", "gray90"),
+                hover_color=("gray70", "gray30"),
+                image=chat_image,
+                anchor="w",
+            )
+            chat_button.grid(row=2, column=0,pady=(0,0), sticky="ew")
+            
+            add_button_friend = ctk.CTkButton(
+                navigation_frame,
+                corner_radius=0,
+                height=40,
+                border_spacing=10,
+                text="Adicionar amigo",
+                fg_color="transparent",
+                text_color=("gray10", "gray90"),
+                hover_color=("gray70", "gray30"),
+                image=img_add_user,
+                anchor="w",
+            )
+            add_button_friend.grid(row=3, column=0,pady=(0,0), sticky="ew")
+            
+            def options_menu(escolha):
+                if escolha == "Sair":
+                    msg_exit = CTkMessagebox(
+                        title="Sair?",
+                        message="Deseja sair do programa?",
+                        icon="question",
+                        option_1="Cancelar",
+                        option_2="Sair e voltar para tela de login",
+                        option_3="Sair e fechar programa",
+                        button_color="Red",
+                        button_hover_color="#b30000",
+                        button_width=40,
+                        width=440,
+                    )
+
+                    response = msg_exit.get()
+
+                    if response == "Sair e fechar programa":
+                        window.destroy()
+                        screen_menu.destroy()
+
+                    elif response == "Sair e voltar para tela de login":
+                        # > deiconify() serve para mostrar a tela que tinha sido escondida
+                        window.deiconify()
+                        screen_menu.destroy()
+
+            label_img_menu = ctk.CTkLabel(
+                master=navigation_frame, text="", image=img_options_menu
+            )
+            label_img_menu.place(x=20, y=630)
+            
+            label_user = ctk.CTkLabel(
+                master=navigation_frame, text="User"
+            )
+            label_user.place(x=65, y=630)
+            
+            label_user = ctk.CTkLabel(
+                master=navigation_frame, text="E-mail@gmail.com",font=("Arial", 10, "underline")
+            )
+            label_user.place(x=65, y=650)
+
+            combobox = ctk.CTkOptionMenu(
+                master=navigation_frame,
+                values=["Configurações", "Sair"],
+                command=options_menu,
+            )
+            combobox.grid(row=5, column=0, padx=20, pady=(365,50), sticky="s")
+            combobox.set("Opções")
 
         def screen_register():
             # apagar tela do login e mostar a de cadastro
